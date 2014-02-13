ed34c06111bc5c656642fd1dbe31d20141eb1424
Reinhard Vuks <reinhardvuks@gmail.com>
2014-02-13 15:11:30 +0200

    Update ennusta_disain.php
    panin aja ja kuupäeva kontrolli ka juurde

--- ennusta_disain.php
+++ ennusta_disain.php
@@ -29,35 +29,54 @@
 			<h2 class="tiitel"><a href="#"> Ennusta </a></h2>
 			<div >
 				<form action="salvesta_tulemused.php" method="post">
-
 				<?php
 
 				include "mangude_listid.php";
-
 				$nimed = array("jalgpall","vorkpall", "korvpall", "boonus");
-
 				$n = 1;
+				$m = 0;
+
+				$aeg_live = date('d.m H.i');
+				$aeg_live[7] = (int)$aeg_live[7] + 1;
+
 				for ($j = 0; $j <= sizeof($nimed)-1; $j++){
 					$ala_nimetus = $nimed[$j];
 					$ala_list = spordiala($ala_nimetus);
 					for ($i = 1; $i <=sizeof($ala_list)-1; $i++) {
 					if ($ala_nimetus == "jalgpall"){
 						echo $ala_list[$i]. "<br>";
-						echo "<input name='tulemus$n' type='radio' value='1'/>1<input name='tulemus$n' type='radio' value='x'/>x<input name='tulemus$n' type='radio' value='2'/>2<br />";
-				    } elseif ($ala_nimetus == "boonus"){
+						if (toimumis_aeg()[$m] < $aeg_live){
+							echo "Mäng on alanud või läbi". '<br>';
+							
+						} else {
+							echo "<input name='tulemus$n' type='radio' value='1'/>1<input name='tulemus$n' type='radio' value='x'/>x<input name='tulemus$n' type='radio' value='2'/>2<br />";
+						}
+					} elseif ($ala_nimetus == "boonus"){
 						echo $ala_list[$i]. "<br>";
-				        echo "<input name='tulemus$n' type='text'><br />";
+						if (toimumis_aeg()[$m] < $aeg_live){
+							echo "Mäng on alanud või läbi". '<br>';
+						} else {
+							echo "<input name='tulemus$n' type='text' ><br />";
+						}
 					} elseif ($ala_nimetus == "korvpall") {
 						echo $ala_list[$i]. "<br>";
-				        echo "<input name='tulemus$n' type='radio' value='1'/>1<input name='tulemus$n' type='radio' value='2'/>2<br />";
+						if (toimumis_aeg()[$m] < $aeg_live){
+							echo "Mäng on alanud või läbi". '<br>';
+						} else {
+							echo "<input name='tulemus$n' type='radio' value='1'/>1<input name='tulemus$n' type='radio' value='2'/>2<br />";
+						}
 					} elseif ($ala_nimetus == "vorkpall") {
 						echo $ala_list[$i]. "<br>";
-				        echo "<input name='tulemus$n' type='radio' value='1'/>1<input name='tulemus$n' type='radio' value='2'/>2<br />";
+						if (toimumis_aeg()[$m] < $aeg_live){
+							echo "Mäng on alanud või läbi". '<br>';
+						} else {
+							echo "<input name='tulemus$n' type='radio' value='1'/>1<input name='tulemus$n' type='radio' value='2'/>2<br />";
+						}
 					}
 					$n += 1;
+					$m += 1;
 					}
 				}
-
 				?>
 				<input type="submit">
 				</form>
